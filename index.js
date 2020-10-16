583c36168ce72e17feec633d3137df2d8dba8971
craig olonyi <lawrence.olonyi.67@gmail.com>
2020-10-17 00:54:37 +0300

    add chicken Hawaiin

--- index.js
+++ index.js
@@ -11,3 +11,40 @@ var bbqCPreferences = [];
 var bbqCPrices = [];
 var bbqSPreferences = [];
 var bbqSPrices = [];
+
+sessionStorageValue1 = [];
+sessionStorageValue2 = [];
+sessionStorageValue3 = [];
+sessionStorageValue4 = [];
+sessionStorageValue5 = [];
+sessionStorageValue6 = [];
+sessionStorageValue7 = [];
+sessionStorageValue8 = [];
+sessionStorageValue9 = [];
+sessionStorageValue10 = [];
+sessionStorageValue11 = [];
+sessionStorageValue12 = [];
+
+//chicken Hawaiin
+function chickenHawaiin(size,crust,toppings){
+  this.size=size;
+  this.crust=crust;
+  this.toppings=toppings;
+  this.type= "Chicken Hawaiin";
+}
+
+chickenHawaiin.prototype.description=function(){
+  return this.type + "," + this.size + "," + this.crust + "," + this.toppings;
+}
+//BBQ Steak
+function bbqSteak(size,crust,toppings){
+  this.size=size;
+  this.crust=crust;
+  this.toppings=toppings;
+  this.type= "BBQ Steak";
+}
+
+//Chicken Tika
+//Boerewors
+//Margheritta
+//Meatzza
