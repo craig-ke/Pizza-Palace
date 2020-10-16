b11861ab5b48e45def9354adeee6a40cf319e0eb
craig olonyi <lawrence.olonyi.67@gmail.com>
2020-10-16 23:47:50 +0300

    add variables

---
+++ index.js
@@ -0,0 +1,13 @@
+var inputtedSize;
+var inputtedTopping;
+var inputtedCrust;
+var pizzaPrice;
+var pizzaType;
+var pizzaDescription;
+var totalPrice = 0;
+var checkoutCount = 0;
+
+var bbqCPreferences = [];
+var bbqCPrices = [];
+var bbqSPreferences = [];
+var bbqSPrices = [];
